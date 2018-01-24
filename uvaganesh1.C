3c47d78b90238bf44a09fa3695f3653820f37e58
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-01-24 16:56:15 +0530

    uvaganesh1

---
+++ uvaganesh1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the  number",a);
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("the number is even");
+	else
+	printf("the number is odd");
+	return 0;
+}
